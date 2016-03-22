f0f5dbc8c46a20bd646e9b1231e53d413e7c5001
TransientCW <flyinwilson@hotmail.com>
2016-03-21 18:25:00 -0700

    added db

--- webroot/pages/populateDetailViews.php
+++ webroot/pages/populateDetailViews.php
@@ -1,8 +1,20 @@
 <?php
+$type = $_GET['type'];
+$id = $_GET['id'];
+
+if($type == 'internship') {
+    populate_internship_detail($id);
+}
+elseif($type == 'organization') {
+    populate_org_detail($id);
+}
+elseif($type == 'student') {
+    populate_org_detail($id);
+}
+
 
 function populate_internship_detail($val) {
     $mysqli = get_db_connection();
-
     if($result = $mysqli->query("CALL internship_detail_single($val)")) {
         echo "<h1>$result</h1>";
     }
@@ -12,16 +24,18 @@ function populate_org_detail($val) {
     $mysqli = get_db_connection();
 
     if($result = $mysqli->query("CALL org_detail_single($val)")) {
-        echo "<ul>";
-        foreach($result as $row) {
-
-            foreach($row as $element) {
-                echo "<li>$element</li>";
-            }
-        }
-        echo "</ul>";
+        echo "<h1>$result</h1>";
     }
 }
+
+function populate_student_detail($val) {
+    $mysqli = get_db_connection();
+    if($result = $mysqli->query("CALL student_detail_single($val)")) {
+        echo "<h1>$result</h1>";
+    }
+}
+
+
 
 function get_db_connection() {
     include 'db_connect.php';
